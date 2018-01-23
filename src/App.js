6aaa9231b6bffd6c24f5dfd6f08918fcaae27e8d
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-01-23 15:10:27 +0200

    topojson data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
 import { geoMercator, geoPath } from "d3-geo";
-import worlddata from "./world";
+import { feature } from "topojson-client";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      worldData: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://unpkg.com/world-atlas@1.1.4/world/110m.json")
+      .then(response => response.json())
+      .then(worldData =>
+        this.setState({
+          worldData: feature(worldData, worldData.objects.countries).features
+        })
+      )
+      .catch(err => console.error(err));
+  }
+
   render() {
     const projection = geoMercator();
     const pathGenerator = geoPath().projection(projection);
-    const countries = worlddata.features.map((d, i) => (
+    const countries = this.state.worldData.map((d, i) => (
       <path key={"path" + i} d={pathGenerator(d)} className="countries" />
     ));
     return (
-      <svg width={1001} height={500}>
+      <svg width={800} height={450}>
         {countries}
       </svg>
     );
+
+    
   }
 }
 
