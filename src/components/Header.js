e854cd4839ccf1f0ee78b23676090fda49b08382
tarmeli <tommi.tarkki@gmail.com>
2018-02-01 12:16:28 +0200

    added Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <section className="Hero">
+        <h1 className="title">World Map</h1>
+        <h2 className="subtitle">By Woahtek</h2>
+      </section>
+    );
+  }
+}
+
+export default Header;
