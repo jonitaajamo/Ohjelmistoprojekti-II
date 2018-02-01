f2b7042733d41c9e023d3a858ab688a3cac475dd
tarmeli <tommi.tarkki@gmail.com>
2018-02-01 15:08:15 +0200

    added timeline placeholder and example news articles

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,27 +4,39 @@ import Currency from "./content/Currency";
 import StockMarket from "./content/StockMarket";
 import Portfolio from "./content/Portfolio";
 import Country from "./content/Country";
+import News from "./content/News";
 
 class Content extends Component {
   render() {
+
+    let contentStyle = {
+      width: "99%",
+      margin: "auto"
+    }
+
     return (
-      <div class="tile is-ancestor">
-        <div class="tile is-vertical is-8">
-          <div class="tile is-parent">
+      <div style={contentStyle} className="tile is-ancestor">
+        <div className="tile is-vertical is-8">
+          <div className="tile is-parent">
             <Map />
           </div>
-          <div class="tile">
-            <div class="tile is-parent is-vertical">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-danger">
+              <p className="subtitle">Timeline placeholder</p>
+            </article>
+          </div>
+          <div className="tile">
+            <div className="tile is-parent is-vertical">
               <Currency />
               <StockMarket />
             </div>
-            <div class="tile is-parent">
+            <div className="tile is-parent">
               <Portfolio />
             </div>
           </div>
         </div>
-        <div class="tile is-parent">
-          <Country />
+        <div className="tile is-parent">
+          <News />
         </div>
       </div>
     );
