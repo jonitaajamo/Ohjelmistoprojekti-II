057dd86f3b1e16b8819a9403dd9380849528398a
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-02-01 12:38:41 +0200

    prettify tooltip

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const Tooltip = ({country}) => (
-  <div className="tooltip">
-      <div>
-        <span>Portfolio</span>
-        <span>Pörssi</span>
-        <span>Valuutta</span><br/>
+const Tooltip = ({ country }) => (
+  <div className="box">
+    <div className="media-content">
+      <div className="content">
+        <span>Portfolio </span>
+        <span>Pörssi </span>
+        <span>Valuutta </span>
+        <br />
         <span>You clicked {country}</span>
       </div>
+    </div>
   </div>
 );
 
