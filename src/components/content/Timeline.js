af8d8e99fede4ec92e17e8938305f8f5bfb56226
tarmeli <tommi.tarkki@gmail.com>
2018-02-13 08:13:45 +0200

    Timeline fix

--- src/components/content/Timeline.js
+++ src/components/content/Timeline.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const StockMarket = () => (
+const Timeline = () => (
   <article className="tile is-child notification is-danger">
     <input className="progress" type="range"></input>
   </article>
 );
 
-export default StockMarket;
+export default Timeline;
