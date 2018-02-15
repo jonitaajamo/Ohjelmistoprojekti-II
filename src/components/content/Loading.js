f276b7e9f5117d9dff4aaacf094a3c9d5982a9df
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-02-15 13:46:37 +0200

    loading, heatdata

---
+++ src/components/content/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactLoading from "react-loading";
+
+const Loading = ({color}) => (
+  <ReactLoading type={"bubbles"} color={color} height={"600"}/>
+)
+
+export default Loading
