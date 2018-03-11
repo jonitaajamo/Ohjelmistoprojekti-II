eb6233b78ade990b681d05ea03c5d331d4133c51
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-03-11 21:57:56 +0200

    more tests

--- src/tests/Country.test.js
+++ src/tests/Country.test.js
@@ -2,7 +2,19 @@ import React from "react";
 import Country from "../components/content/Country";
 import { shallow } from "enzyme";
 
-it("displays country name", () => {
-  const wrapper = shallow(<Country country="Finland" />);
-  expect(wrapper).toMatchSnapshot();
+describe("Country", () => {
+  it("renders correctly", () => {
+    const wrapper = shallow(<Country />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("displays default text when no country is selected", () => {
+    const wrapper = shallow(<Country />);
+    expect(wrapper.find("p").text()).toEqual("Select a country");
+  });
+
+  it("displays country name", () => {
+    const wrapper = shallow(<Country country="Finland" />);
+    expect(wrapper.find("p").text()).toEqual("Finland");
+  });
 });
