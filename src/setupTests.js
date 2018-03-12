d0865a1184ec7cf375843543716fffc8cc733820
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-03-12 09:41:09 +0200

    fixed fetch error in tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,4 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-global.fetch = require('jest-fetch-mock');
+global.XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
