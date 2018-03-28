0008364b07b94f1098dc3e7ce655b7fec523f32c
Joni Taajamo <joni.taajamo@outlook.com>
2018-03-28 14:09:10 +0300

    style changes and play button

--- src/components/content/Assets.jsx
+++ src/components/content/Assets.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Assets = () => (
-  <article className="tile is-child notification is-primary">
+  <article className="tile is-child notification">
     <p className="title">Assets</p>
     <p className="subtitle">Assets in selected country</p>
   </article>
