4b3987b7178458d7946b65574a60d0aaa3befbcd
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-04-04 13:26:44 +0300

    snapshot update

--- src/tests/Content.test.js
+++ src/tests/Content.test.js
@@ -5,12 +5,7 @@ describe("Content", () => {
   it("renders correctly", () => {
     const wrapper = shallow(<Content />);
     expect(wrapper).toMatchSnapshot();
-  });
-
-  it("renders correctly with children", () => {
-    const wrapper = mount(<Content />);
-    expect(wrapper.find("Map").length).toEqual(1);
-    expect(wrapper.find("Assets").length).toEqual(1);
+    expect(wrapper.find("Map").length).toBe(1);
   });
 
   it("fetches data when mounted", () => {
