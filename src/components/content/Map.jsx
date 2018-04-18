bc89c6f0befa9aba18b0abf71474afe894eeb217
vainva <valtterivainio@hotmail.com>
2018-04-18 13:15:41 +0300

    panning disabled

--- src/components/content/Map.jsx
+++ src/components/content/Map.jsx
@@ -244,6 +244,7 @@ class Map extends Component {
                   <ZoomableGroup
                     center={this.state.mapCenter}
                     zoom={this.state.mapZoomValue}
+                    disablePanning="true"
                   >
                     {mapGeographies}
                   </ZoomableGroup>
@@ -254,7 +255,7 @@ class Map extends Component {
                     className="button"
                     onClick={() => this.zoomOutOfGeography()}
                   >
-                    Zoom out
+                    Reset
                   </button>
                 </div>
               </div>
